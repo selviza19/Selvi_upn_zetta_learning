99744f8b6a4c56d4d5529742d7a248a02a805131
selviza19 <100017665+selviza19@users.noreply.github.com>
2022-10-24 22:41:07 +0700

    Selesai js day1

---
+++ jsday1.js
@@ -0,0 +1,30 @@
+/**
+ * @param book 
+ * @param discount
+ * @param tax
+ */
+
+ function bookPurchasing(book, discount, tax)
+ {
+     let result = 0;
+     if(book && book.price)
+     {
+         const price = book.price;
+         const AmountofDiscount  = price *(discount/100);
+         const PriceafterDiscount = price - AmountofDiscount;
+         const Tax = PriceafterDiscount * (tax/100);
+         const PriceTax = PriceafterDiscount  - Tax;
+ 
+         console.group();
+         console.log('Price : Rp.', price);
+         console.log('Discount', discount,'%');
+         console.log('Tax', tax,'%');
+         console.log('Price after discount : Rp.', PriceafterDiscount);
+         console.log('Tax deductible price : Rp.', PriceTax);
+         console.log('Total price : Rp.', PriceTax);
+         console.groupEnd();
+         
+     }
+     return result;
+ }
+ bookPurchasing({title: 'Love Story', price: 10000, printing_status: true}, 10, 10);
