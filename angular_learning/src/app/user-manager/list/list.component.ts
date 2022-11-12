33f70578959fc0702e004f1d09500ac09ca82bf9
selviza19 <100017665+selviza19@users.noreply.github.com>
2022-11-13 00:48:21 +0700

    Selsai angular day7

---
+++ angular_learning/src/app/user-manager/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User, UserManagerService } from '../user-manager.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  public list : Observable<User[]>
+  constructor(private service : UserManagerService) {
+    this.list = this.service.users$
+   console.log(this.list);
+   }
+
+  ngOnInit(): void {
+  }
+
+}
